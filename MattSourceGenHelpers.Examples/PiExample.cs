acabbc18c41cd2b3136f4184fd52c87718dee17e
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-02-26 22:01:11 +0100

    PiExample WIP

---
+++ MattSourceGenHelpers.Examples/PiExample.cs
@@ -0,0 +1,14 @@
+﻿using MattSourceGenHelpers.Abstractions;
+
+namespace MattSourceGenHelpers.Examples;
+
+public static class PiExample
+{
+    /// <summary>
+    /// Returns Nth Pi decimal number. e.g. for 1 return 1, for 2 it returns 4, etc.
+    /// </summary>
+    public partial static int GetPiDecimal(int decimalNumber);
+    
+    [GeneratesMethod(nameof(GetPiDecimal))]
+    static Func<int> GetPiDecimal_Generator(int decimalNumber);
+}
