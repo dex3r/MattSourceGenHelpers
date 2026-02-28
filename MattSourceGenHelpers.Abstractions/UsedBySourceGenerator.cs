c40d51f3b4c3f41b71980c367b26dba8dde26967
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-02-28 23:29:45 +0100

    Removed unused code

--- MattSourceGenHelpers.Abstractions/UsedBySourceGenerator.cs
+++
@@ -1,7 +0,0 @@
-﻿using JetBrains.Annotations;
-
-namespace MattSourceGenHelpers.Abstractions;
-
-[AttributeUsage(AttributeTargets.All)]
-[MeansImplicitUse(ImplicitUseKindFlags.Access | ImplicitUseKindFlags.Access | ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature, ImplicitUseTargetFlags.WithMembers)]
-public class UsedBySourceGenerator : Attribute;
