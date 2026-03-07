4dc9cc3257d3524bc5c99f4f32b6beaa158dc1de
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-03-07 14:30:16 +0100

    WIP

---
+++ EasySourceGenerators.GeneratorTests/SimpleMethodWithParameter.cs
@@ -0,0 +1,27 @@
+﻿namespace EasySourceGenerators.GeneratorTests;
+
+public class SimpleMethodWithParameterTests
+{
+    [Test]
+    public void SimpleMethodWithParameterTests_Test()
+    {
+        string source = """
+                        using EasySourceGenerators.Abstractions;
+
+                        namespace TestNamespace;
+
+                        public partial class SimpleMethodWithParameterClass
+                        {
+                            public partial int SimpleMethodWithParameter(int someIntParameter);
+
+                            [GeneratesMethod(sameClassMethodName: nameof(SimpleMethodWithParameter))]
+                            private static int SimpleMethodWithParameter_Generator(int someIntParameter)
+                            {
+                                return 5;
+                            }
+                        }
+                        """;
+
+        //TODO: This should not compile and throw error MSGH007
+    }
+}
