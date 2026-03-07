c2e6601834ee0d8e2f7aa6d61f2ae0cde5f9d433
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-03-07 13:34:50 +0100

    Added ConsoleAppTests

---
+++ EasySourceGenerators.ConsoleAppTests/ColorsClass.cs
@@ -0,0 +1,17 @@
+﻿using EasySourceGenerators.Abstractions;
+
+namespace EasySourceGenerators.ConsoleAppTests;
+
+public enum ColorsEnum
+{ 
+    Red, Green, Blue
+}
+
+public partial class ColorsClass
+{
+    public partial string GetAllColorsString();
+
+    [GeneratesMethod(nameof(GetAllColorsString))]
+    static string GetAllColorsString_Generator() =>
+        string.Join(", ", Enum.GetNames<ColorsEnum>());
+}
---
+++ EasySourceGenerators.ConsoleAppTests/ColorsClassFluent.cs
@@ -0,0 +1,23 @@
+﻿using EasySourceGenerators.Abstractions;
+
+namespace EasySourceGenerators.ConsoleAppTests;
+
+public partial class ColorsClassFluent
+{
+    public partial string GetAllColorsString();
+
+    [GeneratesMethod(nameof(GetAllColorsString))]
+    static IMethodImplementationGenerator GetAllColorsString_Generator() =>
+        Generate
+            .Method().WithReturnType<string>()
+            .UseBody(() => string.Join(", ", Enum.GetNames<ColorsEnum>()));
+}
+
+/*
+ This will generate the following method:
+
+    public string GetAllColorsString()
+    {
+        return "Red, Green, Blue";
+    }
+*/
