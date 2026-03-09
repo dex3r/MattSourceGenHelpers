cec4127f8083fc06a10062fd136bf5a1167a99b2
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-03-09 20:05:37 +0100

    TemplateEngine.RunAtCompileTime WIP

---
+++ EasySourceGenerators.Abstractions/MethodTemplate.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Annotations;
+
+namespace EasySourceGenerators.Abstractions;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+[MeansImplicitUse(ImplicitUseKindFlags.Access, ImplicitUseTargetFlags.WithMembers)]
+public class MethodTemplate : Attribute;
---
+++ EasySourceGenerators.Examples/PiExampleWithTemplate.cs
@@ -0,0 +1,36 @@
+﻿using EasySourceGenerators.Abstractions;
+
+namespace EasySourceGenerators.Examples;
+
+public static partial class PiExampleWithTemplate
+{
+    public static partial int GetPiDecimal(int decimalNumber);
+
+    [GeneratesMethod(nameof(GetPiDecimal))]
+    [MethodTemplate]
+    static int GetPiDecimal_Template(int decimalNumber)
+    {
+        switch (decimalNumber)
+        {
+            case 0: return TemplateEngine.RunAtCompileTime(() => SlowMath.CalculatePiDecimal(0));
+            case 1: return TemplateEngine.RunAtCompileTime(() => SlowMath.CalculatePiDecimal(1));
+            case 2: return TemplateEngine.RunAtCompileTime(() => SlowMath.CalculatePiDecimal(2));
+            default: return CalculatePiDecimal(decimalNumber);
+        }
+    }
+}
+
+/*
+ This will generate the following method:
+
+    public static int GetPiDecimal(int decimalNumber)
+    {
+        switch (decimalNumber)
+        {
+            case 0: return 3;
+            case 1: return 1;
+            case 2: return 4;
+            default: return CalculatePiDecimal(decimalNumber);
+        }
+    }
+*/
